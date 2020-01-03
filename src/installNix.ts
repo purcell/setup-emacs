288400557b2800c942bee5f5e990f8e9768a25b1
Steve Purcell <steve@sanityinc.com>
2020-01-04 10:43:45 +1300

    Work around Nix installation woes on Catalina
    See https://github.com/purcell/setup-emacs/issues/3

--- src/installNix.ts
+++ src/installNix.ts
@@ -2,7 +2,7 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 import * as tc from "@actions/tool-cache";
-import { homedir, userInfo } from "os";
+import { homedir, userInfo, type } from "os";
 import { existsSync } from "fs";
 
 export async function run() {
@@ -11,6 +11,33 @@ export async function run() {
     const { username } = userInfo();
     const PATH = process.env.PATH;
     const CERTS_PATH = home + "/.nix-profile/etc/ssl/certs/ca-bundle.crt";
+
+    // Catalina workaround https://github.com/NixOS/nix/issues/2925
+    if (type() == "Darwin") {
+      const INSTALL_PATH = "/opt/nix";
+      await exec.exec("sudo", [
+        "sh",
+        "-c",
+        `echo \"nix\t${INSTALL_PATH}\"  >> /etc/synthetic.conf`
+      ]);
+      await exec.exec("sudo", [
+        "sh",
+        "-c",
+        `mkdir -m 0755 ${INSTALL_PATH} && chown runner ${INSTALL_PATH}`
+      ]);
+      await exec.exec(
+        "/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util",
+        ["-B"]
+      );
+
+      // Needed for sudo to pass NIX_IGNORE_SYMLINK_STORE
+      await exec.exec("sudo", [
+        "sh",
+        "-c",
+        "echo 'Defaults env_keep += NIX_IGNORE_SYMLINK_STORE'  >> /etc/sudoers"
+      ]);
+      core.exportVariable("NIX_IGNORE_SYMLINK_STORE", "1");
+    }
 
     // Workaround a segfault: https://github.com/NixOS/nix/issues/2733
     await exec.exec("sudo", ["mkdir", "-p", "/etc/nix"]);
